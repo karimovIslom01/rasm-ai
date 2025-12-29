e070fe0d92de5d18120a47e8104f9e43bdb2b803
karimovIslom01 <karimovislom718@gmail.com>
2025-12-29 19:16:47 +0500

    Fix duplicate base property in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/rasm-ai/', // 🔥 REPO NOMI BILAN AYNAN BIR XIL
   plugins: [react()],
-  base: '/rasm-ai/'
 })
